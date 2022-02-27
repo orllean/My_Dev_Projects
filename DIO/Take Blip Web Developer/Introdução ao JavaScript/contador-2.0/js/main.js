a470e426ee2306e9952e1db5d101237aa8f19367
Orlean <orllean@gmail.com>
2022-02-27 20:18:46 -0300

    add max e min para contador js

--- DIO/Take Blip Web Developer/Introdução ao JavaScript/contador-2.0/js/main.js
+++ DIO/Take Blip Web Developer/Introdução ao JavaScript/contador-2.0/js/main.js
@@ -1,18 +1,22 @@
 let count = 0;
-const NUMBER = document.getElementById('number')
-document.getElementById('add').addEventListener("click", increment);
-document.getElementById('sub').addEventListener("click", decrement);
+const NUMBER = document.getElementById('number');
+const ADD = document.getElementById('add');
+const SUB = document.getElementById('sub');
+ADD.addEventListener("click", increment);
+SUB.addEventListener("click", decrement);
 
 function increment() {
     count++;
     NUMBER.innerHTML = count;
     colorNumber (count);
+    switchButtum (count)    
 }
 
 function decrement() {
     count--;
     NUMBER.innerHTML = count;
     colorNumber (count);
+    switchButtum (count)    
 }
 
 function colorNumber (num) {
@@ -23,4 +27,27 @@ function colorNumber (num) {
     } else {
         NUMBER.style.color = 'black';        
     }    
+}
+
+function switchButtum (num) {
+    if (count == 10) {
+        ADD.disabled = true;
+        ADD.style.cursor = 'default';
+        ADD.style.border = 'none';
+    } else {
+        ADD.disabled = false;
+        ADD.style.cursor = 'pointer';
+        ADD.style.border = 'solid';
+
+    }
+    if (count == -10) {
+        SUB.disabled = true;
+        SUB.style.cursor = 'default';
+        SUB.style.border = 'none';
+    } else {
+        SUB.disabled = false;
+        SUB.style.cursor = 'pointer';
+        SUB.style.border = 'solid';
+
+    }
 }
