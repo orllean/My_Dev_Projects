a95aa12a90e0ebfe974e9e3fca715f9e11ebdc7d
Orlean <orllean@gmail.com>
2022-06-21 19:16:34 -0300

    finish js module

---
+++ DIO/Santander Bootcamp Fullstack Developer/Trabalhando com Módulos em JavaScript/modulos/pratica/main.mjs
@@ -0,0 +1,5 @@
+import { mostrarIdede, mostrarCidade, mostrarHobby } from "./funcoes.mjs";
+
+console.log(mostrarIdede("papai", 50));
+console.log(mostrarIdede("papai", "salvador"));
+console.log(mostrarIdede("papai", "tocar violão"));
