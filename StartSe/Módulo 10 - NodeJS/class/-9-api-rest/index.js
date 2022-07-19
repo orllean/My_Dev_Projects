ec00f8845aa73df2952523eae5632b33d9cdec28
Orlean <orllean@gmail.com>
2022-07-19 15:59:05 -0300

    finish class express server

--- StartSe/Módulo 10 - NodeJS/class/-9-api-rest/index.js
+++ StartSe/Módulo 10 - NodeJS/class/-9-api-rest/index.js
@@ -1,2 +1,12 @@
-import express from 'express'
+import express from "express";
 
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+	console.log(`servidor rodando na em http://localhost:${PORT}`);
+});
+
+app.get("/", (request, response) => {
+	return response.send("<h1>Trabalhando com servidor Express</h1>");
+});
