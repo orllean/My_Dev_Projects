77396662479b1cebd2c12d47408a8b3235ea7586
Orlean <orllean@gmail.com>
2022-08-01 20:21:21 -0300

    update project

--- StartSe/Módulo 10 - NodeJS/class/-9-api-rest/index.js
+++ StartSe/Módulo 10 - NodeJS/class/-9-api-rest/index.js
@@ -2,11 +2,18 @@ import express from "express";
 
 const app = express();
 const PORT = 3000;
-
+let users = [
+	{ id: 1, name: "Rafael Ribeiro", age: 31 },
+	{ id: 2, name: "Orlean Costa", age: 51 },
+];
 app.listen(PORT, () => {
 	console.log(`servidor rodando na em http://localhost:${PORT}`);
 });
 
 app.get("/", (request, response) => {
 	return response.send("<h1>Trabalhando com servidor Express</h1>");
+});
+
+app.get("/users", (request, response) => {
+	return response.send(users);
 });
